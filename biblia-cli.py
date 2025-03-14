7458c79df7db48d987cd6941cfaf258e2f43b5cd
= <=>
2025-03-13 22:28:10 -0300

    Arquivo da Bíblia em TXT

--- biblia-cli.py
+++ biblia-cli.py
@@ -1,6 +1,8 @@
 
-caminho_biblia = '/Biblia CLI/biblia.txt'
+caminho_biblia = '/Biblia-CLI/biblia.txt'
 
-with open(caminho_biblia, 'r') as arquivo_biblia:
+with open(caminho_biblia, 'r', encoding='utf-8') as arquivo_biblia:
     biblia = arquivo_biblia.readlines()
 
+titulo = ''.join(biblia[:3]).strip()
+print(titulo)
